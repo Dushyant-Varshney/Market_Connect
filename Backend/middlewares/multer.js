9fda1096f742a75d74e69d6cbb40e25ad86581e3
ckrysna <202301439@github.com>
2025-11-04 20:03:58 +0530

    customer service css

--- Backend/middlewares/multer.js
+++
@@ -1,7 +0,0 @@
-//Multer handles image upload by user and then upload in cloud from backend
-const multer = require('multer');
-const { storage } = require('../CloudConfig');
-
-const upload = multer({ storage });
-
-module.exports = upload;
